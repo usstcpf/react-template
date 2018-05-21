a8995f25b210e23d4a8411340d090cd7f73af272
Pufang Cheng <928419109@qq.com>
2018-05-21 23:42:39 +0800

    init sping boot env

--- src/main/java/com/cpf/reacttemplate/ReactTemplateApplication.java
+++ src/main/java/com/cpf/reacttemplate/ReactTemplateApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReactTemplateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReactTemplateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ReactTemplateApplication.class, args);
+    }
 }
---
+++ src/main/java/com/cpf/reacttemplate/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.cpf.reacttemplate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "hello world";
+    }
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
