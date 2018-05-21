06c1dbc9c3be8b3b6904b6b3a63a6f062195ef38
Pufang Cheng <928419109@qq.com>
2018-05-21 22:51:59 +0800

    init spring boot env

---
+++ src/main/java/com/cpf/reacttemplate/ReactTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.cpf.reacttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactTemplateApplication.class, args);
+	}
+}
